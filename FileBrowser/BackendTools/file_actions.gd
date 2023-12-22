9fbbe1f6f1124c2ad2a2487aa7e23e6336a5b123
userhaj <hagerja@gmail.com>
2023-12-21 16:00:53 -0800

    Add recursive deep copying/moving of files and folder structures

--- FileBrowser/BackendTools/file_actions.gd
+++ FileBrowser/BackendTools/file_actions.gd
@@ -16,14 +16,20 @@ static func _copy(files: PackedStringArray, target_folder: String, percent_callb
 	var file_count = float(len(files))
 	var files_complete = 0.0
 	for file: String in files:
-		# If from is same as to in "move", do nothing
-		if file.get_base_dir().simplify_path() == target_folder.simplify_path():
-			break
-			
+		# Is it a file or a directory?
+		var is_file = FileAccess.file_exists(file)
+		
+		# Is the target folder and the base folder the same?
+		var is_file_in_target = file.get_base_dir().simplify_path() == target_folder.simplify_path()
+		if is_file_in_target:
+			# If from is same as to in "move", do nothing
+			if is_move:
+				break
+		
 		var target_location = target_folder.path_join(file.get_file())
 		
 		# If file exists, overwrite it or rename current?
-		if FileAccess.file_exists(target_location):
+		if FileAccess.file_exists(target_location) or DirAccess.dir_exists_absolute(target_location):
 			var semaphore := Semaphore.new()
 			error_ask_user.call_deferred(ERR_ALREADY_EXISTS, target_location, semaphore)
 			semaphore.wait()
@@ -35,21 +41,45 @@ static func _copy(files: PackedStringArray, target_folder: String, percent_callb
 					break
 				# Rename by appending #
 				FileResponseRequest.RENAME:
-					target_location = FileActions.nearest_available_name(target_location)
+					# Rename target on request
+					if is_file:
+						target_location = FileActions.nearest_available_name(target_location)
+					else:
+						target_location = FileActions.nearest_available_dir_name(target_location)
 				# Move on to next file
 				FileResponseRequest.SKIP:
 					continue
 				# Overwrite old file
 				FileResponseRequest.OVERWRITE:
 					pass
-
-		var dir_access := DirAccess.open(target_folder)
-		var err = dir_access.copy(file, target_location)
-		if err == OK:
-			if is_move:
-				# TODO Items SHOULD be removed, but use trash until all bugs fixed
-				#dir_access.remove(file)
-				OS.move_to_trash(file)
+		# Recursively copy all folders
+		if not is_file:
+			# Copy Internal Files
+			var all_dirs = all_sub_directories_at(file)
+			var target_dirs = all_dirs.duplicate()
+			replace_array(file, target_location, target_dirs)
+			for index in range(len(all_dirs)):
+				copy_files(all_dirs[index], target_dirs[index])
+			
+		
+		if is_file:
+			var dir_access := DirAccess.open(target_folder)
+			var err = dir_access.copy(file, target_location)
+			if err == OK:
+				if is_move:
+					# TODO Items SHOULD be removed, but use trash until all bugs fixed
+					#dir_access.remove(file)
+					OS.move_to_trash(file)
+		else:
+			var err_array: Array = copy_files(file, target_location)
+			var error_sum = err_array.reduce(func(accum, number): return accum + number)
+			if error_sum == 0:
+				if is_move:
+					# TODO Items SHOULD be removed, but use trash until all bugs fixed
+					#dir_access.remove(file)
+					OS.move_to_trash(file)
+			
+			
 		
 		# Notify completion percent
 		files_complete += 1.0
@@ -59,18 +89,68 @@ static func _copy(files: PackedStringArray, target_folder: String, percent_callb
 	percent_callback.call_deferred(1.0)
 	# Call cleanup code on thread
 	FileActions._end_thread.call_deferred(thread)
-
-
+		
 
 # Provides numbered filename that is available
 static func nearest_available_name(file_path: String):
+	# TODO Handle non-extension files
 	var new_name = file_path
 	var count = 1
 	while FileAccess.file_exists(new_name):
 		new_name = file_path.get_basename() + str(count) + "." + file_path.get_extension()
 		count += 1
 	return new_name
-	
+
+# Provides numbered filename that is available
+static func nearest_available_dir_name(dir_path: String):
+	#TODO Handle / or \ at end of dir_path
+	var new_name = dir_path
+	var count = 1
+	while DirAccess.dir_exists_absolute(new_name):
+		new_name = dir_path + str(count)
+		count += 1
+	return new_name
+
 # Call finish code on any thread USAGE: FileActions._end_thread.call_deferred(thread)
 static func _end_thread(thread: Thread):
 	thread.wait_to_finish()
+
+# Recursively get all directories in folder
+static func all_sub_directories_at(path: String) -> Array[String]:
+	var array: Array[String] = []
+	_all_sub_directories_at(path, array)
+	return array
+
+# Recursively get all directories in folder, appends to given array
+static func _all_sub_directories_at(path: String, array: Array):
+	for dir in DirAccess.get_directories_at(path):
+		array.append(path.path_join(dir))
+		_all_sub_directories_at(path.path_join(dir), array)
+
+# Recursively get all directories in folder, appends to given array
+static func all_sub_files_at(path: String) -> Array[String]:
+	var array: Array[String] = []
+	_all_sub_files_at(path, array)
+	return array
+
+# Recursively get all directories in folder, appends to given array
+static func _all_sub_files_at(path: String, array: Array):
+	for dir in DirAccess.get_files_at(path):
+		array.append(path.path_join(dir))
+		_all_sub_files_at(path.path_join(dir), array)
+
+# Replaces all occurrences of what inside the string with the given forwhat, for every item in array
+static func replace_array(what: String, forwhat: String, array: Array[String]):
+	for index in range(len(array)):
+		array[index] = array[index].replace(what, forwhat)
+
+# Shallow copy just files from one folder to target
+static func copy_files(originating_dir: String, target_dir: String)-> Array[Error]:
+	if not DirAccess.dir_exists_absolute(target_dir):
+		DirAccess.make_dir_recursive_absolute(target_dir)
+	var errors: Array[Error] = []
+	var dir_access := DirAccess.open(originating_dir)
+	for file in dir_access.get_files():
+		var err = dir_access.copy(originating_dir.path_join(file), target_dir.path_join(file))
+		errors.append(err)
+	return errors
