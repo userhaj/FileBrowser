2845f852f68494d4e5aa711faa66d9b7f7941839
userhaj <hagerja@gmail.com>
2026-04-21 19:06:41 -0700

    Initial Color Setting Page
    Initial layout for color settings page

---
+++ FileBrowser/Color/single_color_choice_gui.gd
@@ -0,0 +1,26 @@
+extends HBoxContainer
+signal color_changed(color: Color)
+
+var default_value: Color
+var ui_name: String
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func set_default(color: Color):
+	self.default_value = color
+
+func setup(ui_name: String, default: Color)-> void:
+	self.ui_name = ui_name
+	$Label.text = ui_name
+	self.default_value = default
+	$ColorPickerButton.color = default
+
+func _on_color_picker_button_color_changed(color: Color) -> void:
+	emit_signal("color_changed", color)
+
+
+func _on_button_reset_pressed() -> void:
+	$ColorPickerButton.color = self.default_value
---
+++ FileBrowser/color_settings.gd
@@ -0,0 +1,43 @@
+extends Control
+
+signal color_changed
+
+const SAVE_FILEPATH = "user://GDFileBrowserColors.cfg"
+
+const COLOR_DEFAULT = {
+	"Icon View Background": Color(0.198, 0.198, 0.198, 1.0), 
+	"Icon View Font": Color(0.784, 0.784, 0.784, 1.0)
+	}
+
+var color_values: Dictionary = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	load_colors()
+
+func load_colors():
+	var config: ConfigFile = ConfigFile.new()
+	var err = config.load(SAVE_FILEPATH)
+	if err == OK:
+		for ui_name in COLOR_DEFAULT.keys():
+			var ui_color = config.get_value("Colors", ui_name, null)
+			# If color is not found, use default
+			if ui_color == null:
+				ui_color = COLOR_DEFAULT[ui_name]
+			
+			var single_color_gui = preload("res://FileBrowser/Color/single_color_choice_gui.tscn").instantiate()
+			single_color_gui.setup(ui_name, ui_color)
+			single_color_gui.connect("color_changed", a_color_changed)
+			$VBoxContainer.add_child(single_color_gui)
+	else:
+		# If opening config failed, save a new config
+		for ui_name in COLOR_DEFAULT.keys():
+			config.set_value("Color", ui_name, COLOR_DEFAULT[ui_name])
+		config.save(SAVE_FILEPATH)
+
+func a_color_changed():
+	emit_signal("color_changed")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
