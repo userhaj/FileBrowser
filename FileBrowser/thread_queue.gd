713203b943cdd49f43d93b923a67d9dacdea7777
userhaj <hagerja@gmail.com>
2026-05-13 11:44:04 -0700

    Comment ThreadQueue

--- FileBrowser/thread_queue.gd
+++ FileBrowser/thread_queue.gd
@@ -1,5 +1,8 @@
-extends Node
 class_name ThreadQueue
+extends Node
+## A queue for work with a callback of returned value.
+##
+## Maximizes work against available processors, uses callback_content to return value.
 
 var _running_threads = []
 var thread_queue = []
@@ -7,12 +10,14 @@ var bind_queue = []
 
 var max_thread_count = max(2, OS.get_processor_count() * 2 )
 
+# Add Callable a queue. callback_content must take single argument of call_work return type
 func enqueue(call_work: Callable, callback_content:Callable):
 	var thread = Thread.new()
 	thread_queue.push_back(thread)
 	bind_queue.push_back(_thread_return_work.bind(thread, call_work, callback_content))
-	update_queue()
+	_update_queue()
 
+# Runs work, then calls cleanup methods
 func _thread_return_work(thread, call_work, callback_content):
 	if call_work:
 		var work = call_work.call()
@@ -20,17 +25,19 @@ func _thread_return_work(thread, call_work, callback_content):
 		call_deferred("_end_thread", thread, callback)
 	else:
 		call_deferred("_end_thread", thread, null)
-		
+
+# Called upon end of work; removes thread from queue
 func _end_thread(thread:Thread, callback: Callable):
 	var thread_index = self._running_threads.find(thread)
 	if thread_index >= 0:
 		self._running_threads.remove_at(thread_index)
-	update_queue()
+	_update_queue()
 	if callback:
 		callback.call_deferred()
 	thread.wait_to_finish()
 
-func update_queue():
+# Called upon addition or subtraction to queue; starts next thread
+func _update_queue():
 	while len(self._running_threads) < max_thread_count && len(thread_queue) > 0:
 		var run_thread = thread_queue.pop_front()
 		var bind_callable = bind_queue.pop_front()
@@ -39,6 +46,3 @@ func update_queue():
 			run_thread.start(bind_callable)
 		else:
 			break
-			#break
-
-
